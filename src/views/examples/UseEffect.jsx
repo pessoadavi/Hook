47390d469513e68d51ef26499e620d81fa5b3833
Davi Pessoa de Melo <pessoadavi@gmail.com>
2021-01-12 17:07:17 -0300

    useref

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
-const UseEffect = (props) => {
+
+export default (props) => {
+
+    const [number, setNumber] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
+
+    const [numero, setNumero] = useState();
+    const [parOuImpar, setParOuImpar] = useState("Selecione um número.");
+
+
+    function calcFatorial(number) {
+
+        if (number < 0) return "Não existe fatorial de número negativo."
+
+        if (number === 0) return 1;
+
+        return number * calcFatorial(number - 1);
+
+    }
+
+    function calcParOuImpar(numero) {
+
+        let result = numero % 2;
+
+        if (result === 0) {
+            return "Par.";
+        } else {
+            return "Ímpar.";
+        }
+
+    }
+
+    useEffect(() => {                                      // userEffect é utilizado quando se quer que uma função seja acionada
+                                                           // apenas quando a dependência tem alguma alteração. Quando [number]
+        setFatorial(calcFatorial(number))                  // sofrer alteração a função calcFatorial é acionada como um efeito 
+    }, [number]);                                          // colateral da alteração de number.
+
+     
+    useEffect(()=> {
+        setParOuImpar(calcParOuImpar(numero))
+    },[numero]);
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício 01" />
+                <div>
+                    <h1> Fatorial: {fatorial} </h1>
+                    <input type="number" className="input" value={number} onChange={(result) => { setNumber(result.target.value) }} />
+                </div>
+            <SectionTitle title="Exercício 02"/>
+                <div>
+                    <h1>Status: {parOuImpar}</h1>
+                    <input className="input" type="number" onChange={(result)=>{ setNumero(result.target.value)} }></input>
+                </div>
         </div>
     )
 }
 
-export default UseEffect
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+
+    const [value1, setValue1] = useState();
+    const [value2, setValue2] = useState();
+
+    const count = useRef(0);
+    const myinput1 = useRef(null);
+    const myinput2 = useRef(null);
+
+    useEffect(() => {
+        count.current++
+        myinput2.current.focus()
+    }, [value1]);
+
+    useEffect(() => {
+        count.current++
+        myinput1.current.focus()
+    }, [value2]);
+
+    const unify = function(n1, n2){
+
+       return n1 + n2
+    };
+
+
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exercício 01" />
+            <div className="center">
+                <div>
+                    <h2 className="center">Valor: {unify(value1, value2)} {count.current}</h2>
+                    <input ref={myinput1} value={value1} type="text" className="input" onChange={(result) => { setValue1(result.target.value) }} />
+                </div>
+            </div>
+            <SectionTitle title="Exercício 02" />
+                <div>
+                    <h2 className="center"></h2>
+                    <input ref={myinput2} value={value2} type="text" className="input" onChange={(result) => { setValue2(result.target.value) }} />
+                </div>
         </div>
     )
 }
